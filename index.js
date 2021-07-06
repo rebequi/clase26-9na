754fbd43017bfa00f88d0e002c03d141b142f91c
rebequi <rebk1998@icloud.com>
2021-07-05 22:50:17 -0600

    subo clase 26 con primro ejercicios de js

---
+++ index.js
@@ -0,0 +1,79 @@
+// let respuestaDelUser = prompt ('hola como te llamas')
+// console.log(respuestaDelUser)
+// alert ('hola' + ' ' + respuestaDelUser)
+
+// let sumaFavorita = prompt('cual es tu suma favorita')
+// let sumaFavoritaAnumero = (Number(sumaFavorita))
+// console.log (sumaFavoritaAnumero)
+
+// let suma = (2+2)
+// console.log (suma)
+
+
+// Necesitamos un programa que pida un nombre y un apellido con los siguientes mensajes respectivamente:
+
+// ¿Cuál es tu nombre?.
+// ¿Cuál es tu apellido?.
+// Con esta información, el programa deberá mostrar el siguiente mensaje: Hola {nombre} {apellido}, bienvenida a Ada
+
+// let nombre = prompt('¿Cuál es tu nombre?')
+// let apellido = prompt ('¿Cuál es tu apellido?')
+// alert ('Hola' +' '+  nombre +' ' + apellido + ', bienvenida a Ada')
+
+// Necesitamos un programa para una heladería que permita ingresar tres gustos de helado con los siguientes mensajes respectivamente:
+
+// ¿Cuál es el primer gusto?.
+// ¿Cuál es el segundo gusto?
+// ¿Cuál es el tercer gusto?.
+// Con esta información, el programa deberá mostrar el siguiente mensaje: Aquí tiene su helado de {gusto1}, {gusto2} y {gusto3}
+
+
+// let gusto1 = prompt ('¿Cuál es el primer gusto?')
+// let gusto2 = prompt ('¿Cuál es el segundo gusto?')
+// let gusto3 = prompt ('¿Cuál es el tercer gusto?')
+
+// alert ('Aquí tiene su helado de ' + gusto1 +', '+gusto2+ ' y '+ gusto3)
+
+
+
+// Necesitamos un programa que permita ingresar nombre, apellido, edad, nacionalidad y número de documento con 
+
+// los siguientes mensajes respectivamente:
+
+// ¿Cuál es tu nombre?.
+// ¿Cuál es tu apellido?.
+// ¿Cuál es tu apellido?.
+// ¿Cuál es tu edad?
+// ¿Cuál es tu nacionalidad?
+// ¿Cuál es tu número de documento?
+// Con esta información, el programa deberá mostrar el siguiente mensaje: 
+
+// El nuevo perfil fue ingresado al sistema: nombre: {nombre}, apellido: {apellido}, edad: {edad}, nacionalidad: {nacionalidad}, número de documento: {dni}
+
+// let nombre = prompt ('¿Cuál es tu nombre?')
+// let apellido = prompt ('¿Cuál es tu apellido?')
+// let edad = prompt ('¿Cuál es tu edad?')
+// let nacionalidad = prompt ('¿Cuál es tu nacionalidad?')
+// let dni = prompt ('¿Cuál es tu número de documento?')
+
+// alert ('El nuevo perfil fue ingresado al sistema:' + 'nombre: '+ nombre+ 
+// ', apellido: ' + apellido +
+// ', edad: '+ edad + 
+// ', nacionalidad: ' +nacionalidad+
+// ', número de documento: ' + dni)
+
+
+// Nos pidieron un programa que permita ingresar el nombre de una playlist y el título de tres canciones, mediante los siguiente mensajes:
+
+// ¿Cuál es el nombre de la playlist?
+// ¿Cuál es la primera canción?
+// ¿Cuál es la segunda canción?
+// ¿Cuál es la tercera canción?
+// Con está información el programa deberá mostrar el siguiente mensaje: Se ha creado la playlist {playlist} con las canciones {cancion1}, {cancion2}, {cancion3}
+
+// let playlist = prompt ('¿Cuál es el nombre de la playlist?')
+// let cancion1 = prompt ('¿Cuál es la primera canción?')
+// let cancion2 = prompt ('¿Cuál es la segunda canción?')
+// let cancion3 = prompt ('¿Cuál es la tercera canción?')
+
+// alert ('Se ha creado la playlist ' + playlist+ ' con las canciones ' + cancion1 +', '+ cancion2 +', ' +cancion3)
